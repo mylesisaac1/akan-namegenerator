d7ef63e2862200d0db9eb1a254c4b491370dc3bc
mylesisaac1 <mylesisaac2@gmail.com>
2025-04-22 09:32:38 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -51,7 +51,7 @@ function validateDate(day, month, year) {
     // Basic month validation
     if (month < 1 || month > 12) return false;
     
-    // Check valid day for month
+    
     const daysInMonth = new Date(year, month, 0).getDate();
     return day >= 1 && day <= daysInMonth;
 }
